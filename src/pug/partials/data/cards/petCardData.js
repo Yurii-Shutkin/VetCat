1b421b5dc6a88b117cde14bce9aa6d187fcb3501
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-02-03 14:22:56 +0200

    finished stories page adaptive layout

--- src/pug/partials/data/cards/petCardData.js
+++ src/pug/partials/data/cards/petCardData.js
@@ -1,4 +1,5 @@
-module.exports = [
+module.exports = {
+  slider: [
     {
       photoUrl: './img/main/history-1.png',
       title: 'Непростое лечение перелома нижней челюсти у кошки Маруськи',
@@ -37,4 +38,78 @@ module.exports = [
       title: 'Острый панкреатит у йоркширского терьера Луи II Саксонского',
       date: '21.06.2020',
     },
-]
+  ],
+  stories: [
+    {
+      photoUrl: './img/stories/story-1.png',
+      title: 'Непростое лечение перелома нижней челюсти у кошки Маруськи',
+      date: '21.06.2020',
+    },
+    {
+      photoUrl: './img/stories/story-2.png',
+      title: 'Отравление немытым виноградом у пекинеса Гремлина',
+      date: '21.06.2020',
+    },
+    {
+      photoUrl: './img/stories/story-3.png',
+      title: 'Патологические роды и кесарево сечение у крысы Анфисы',
+      date: '21.06.2020',
+    },
+    {
+      photoUrl: './img/stories/story-4.png',
+      title: 'Новообразование мочевого пузыря  у хомяка Бориса',
+      date: '21.06.2020',
+    },
+    {
+      photoUrl: './img/stories/story-5.png',
+      title: 'Язвенный дерматит у кота, отягощенный короновирусной инфекцией',
+      date: '21.06.2020',
+    },
+    {
+      photoUrl: './img/stories/story-6.png',
+      title: 'Отравление немытым виноградом у пекинеса Гремлина',
+      date: '21.06.2020',
+    },
+    {
+      photoUrl: './img/stories/story-7.png',
+      title: 'Культит, осложнённый бактериальным циститом',
+      date: '21.06.2020',
+    },
+    {
+      photoUrl: './img/stories/story-8.png',
+      title: 'Восьмидневные роды и кесарево сечение у Стрелки',
+      date: '21.06.2020',
+    },
+    {
+      photoUrl: './img/stories/story-9.png',
+      title: 'Острый панкреатит у йоркширского терьера Луи II Саксонского',
+      date: '21.06.2020',
+    },
+    {
+      photoUrl: './img/stories/story-10.png',
+      title: 'Доброе дело — история одного котёнка',
+      date: '21.06.2020',
+    },
+    {
+      photoUrl: './img/stories/story-11.png',
+      title: 'Как корги сломала клык в погоне за дружбаном',
+      date: '21.06.2020',
+    },
+    {
+      photoUrl: './img/stories/story-12.png',
+      title: 'Инородное тело в пищеводе собаки',
+      date: '21.06.2020',
+    },
+    {
+      photoUrl: './img/stories/story-13.png',
+      title: 'Билатеральный поликистоз почек у кошки Зинаиды',
+      date: '21.06.2020',
+    },
+    {
+      photoUrl: './img/stories/story-14.png',
+      title: 'Острая почечная недостаточность у пса Мопса',
+      date: '21.06.2020',
+    },
+  ]
+}
+  
