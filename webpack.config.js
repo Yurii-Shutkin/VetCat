c8c475e1cb65f1a4018a47eb8fbe54e2b1462a29
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-02-06 01:02:45 +0200

    modified output path

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].[contenthash].js',
-    publicPath: '/', 
+    publicPath: './', 
     clean: true,
   },
 
